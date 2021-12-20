236b05f6aea9378aff207044c7c1ba3f93186e6d
JJMUNPER <jmunper007@g.educaand.es>
2021-12-20 14:40:50 +0100

    Primer comit desde Apache

---
+++ src/Actividad_01_UD3_ED/Actividad_01_UD3_ED.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Actividad_01_UD3_ED;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author ana
+ */
+public class Actividad_01_UD3_ED 
+{
+    private static byte[] Intermedio = new byte[1000];
+    private static String NombreArchivo = "fichero.dat";
+    private static FileInputStream FlujoEntrada = null;
+    private static BufferedInputStream EntradaIntermedia = null;
+
+    public static void inicializateFiles() throws FileNotFoundException
+    {
+        FlujoEntrada = new FileInputStream(NombreArchivo);
+        EntradaIntermedia = new BufferedInputStream(FlujoEntrada);
+    }
+    
+    public static int showFileText() throws IOException
+    {
+        int total = 0;
+        int nRead = 0;
+        while((nRead = FlujoEntrada.read(Intermedio)) != -1) 
+        {
+            System.out.println(new String(Intermedio));
+            total += nRead;
+        }
+        
+        return total;
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {        
+        try 
+        {
+            inicializateFiles();
+            
+            int total = showFileText();           
+
+            System.out.println("\nLeídos " + total + " bytes");
+        }
+        catch(IOException ex) 
+        {
+            System.out.println("Error -> " + ex.toString());                
+        }
+        finally 
+        {
+            try 
+            {
+                if( EntradaIntermedia != null && FlujoEntrada != null )
+                {
+                    FlujoEntrada.close();
+                    EntradaIntermedia.close();
+                }                
+            } 
+            catch (IOException ex) 
+            {
+                System.out.println("Error al cerrar el fichero -> " + ex.toString());
+            }
+        }
+    }
+    
+}
